8c6a0f8ef3a0082f30253d6203caca7aeb373e3e
Lennert Buytenhek <buytenh@wantstofly.org>
2015-11-26 15:18:32 +0200

    itf: Remove strdup() calls that were there to maintain const correctness.
    Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>

--- itf.c
+++ itf.c
@@ -26,7 +26,7 @@
 #include <unistd.h>
 #include "itf.h"
 
-static int spawnvp(const char *file, char * const *argv)
+static int spawnvp(const char *file, char *const *argv)
 {
 	pid_t pid;
 	int status;
@@ -60,41 +60,31 @@ int itf_add_v6(const char *itf, const uint8_t *addr, int len)
 {
 	char *args[7];
 	char caddr[64];
-	int ret;
 
 	args[0] = "ip";
 	args[1] = "addr";
 	args[2] = "add";
 	args[3] = caddr;
 	args[4] = "dev";
-	args[5] = strdup(itf);
+	args[5] = (char *)itf;
 	args[6] = NULL;
 
 	inet_ntop(AF_INET6, addr, caddr, sizeof(caddr));
 	sprintf(caddr + strlen(caddr), "/%d", len);
 
-	ret = spawnvp("ip", args);
-
-	free(args[5]);
-
-	return ret;
+	return spawnvp("ip", args);
 }
 
 int itf_set_state(const char *itf, int up)
 {
 	char *args[6];
-	int ret;
 
 	args[0] = "ip";
 	args[1] = "link";
 	args[2] = "set";
-	args[3] = strdup(itf);
+	args[3] = (char *)itf;
 	args[4] = up ? "up" : "down";
 	args[5] = NULL;
 
-	ret = spawnvp("ip", args);
-
-	free(args[3]);
-
-	return ret;
+	return spawnvp("ip", args);
 }
