47c133236d9bac3b6404532b83b218f8e1f12f8c
Lennert Buytenhek <buytenh@wantstofly.org>
2016-02-06 00:55:25 +0200

    rib_listener_debug: Print both symbolical and hex IDs for LSA changes.
    Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>

--- rib_listener_debug.c
+++ rib_listener_debug.c
@@ -73,7 +73,11 @@ static void lsa_add(void *cookie, struct lsa *a)
 	struct rib_listener_debug *rl = cookie;
 
 	printf("%s: lsa add [", rl->name);
-	lsa_print_id_name(stdout, a->id, rl->name_hints);
+	if (lsa_print_id_name(stdout, a->id, rl->name_hints)) {
+		printf("(");
+		printhex(stdout, a->id, NODE_ID_LEN);
+		printf(")");
+	}
 	printf("]\n");
 
 	lsa_diff(NULL, a, cookie, attr_add, attr_mod, attr_del);
@@ -86,7 +90,11 @@ static void lsa_mod(void *cookie, struct lsa *a, struct lsa *b)
 	struct rib_listener_debug *rl = cookie;
 
 	printf("%s: lsa mod [", rl->name);
-	lsa_print_id_name(stdout, a->id, rl->name_hints);
+	if (lsa_print_id_name(stdout, a->id, rl->name_hints)) {
+		printf("(");
+		printhex(stdout, a->id, NODE_ID_LEN);
+		printf(")");
+	}
 	printf("]\n");
 
 	lsa_diff(a, b, cookie, attr_add, attr_mod, attr_del);
@@ -99,7 +107,11 @@ static void lsa_del(void *cookie, struct lsa *a)
 	struct rib_listener_debug *rl = cookie;
 
 	printf("%s: lsa del [", rl->name);
-	lsa_print_id_name(stdout, a->id, rl->name_hints);
+	if (lsa_print_id_name(stdout, a->id, rl->name_hints)) {
+		printf("(");
+		printhex(stdout, a->id, NODE_ID_LEN);
+		printf(")");
+	}
 	printf("]\n");
 
 	lsa_diff(a, NULL, cookie, attr_add, attr_mod, attr_del);
