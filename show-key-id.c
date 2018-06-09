d2aeb122b2b378577ee4541ec32920a4f5a3b6b5
Lennert Buytenhek <buytenh@wantstofly.org>
2018-06-09 04:49:49 +0300

    show-key-id: Accept either a private key or a public key.
    Signed-off-by: Lennert Buytenhek <buytenh@wantstofly.org>

--- show-key-id.c
+++ show-key-id.c
@@ -19,22 +19,101 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <gnutls/abstract.h>
 #include <gnutls/x509.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include "util.h"
 #include "x509.h"
 
-static int read_key_id(uint8_t *id, const char *file)
+static int try_privkey(uint8_t *id, uint8_t *buf, int size)
 {
 	gnutls_x509_privkey_t privkey;
+	gnutls_datum_t datum;
 	int ret;
+
+	ret = gnutls_x509_privkey_init(&privkey);
+	if (ret) {
+		fprintf(stderr, "gnutls_x509_privkey_init: ");
+		gnutls_perror(ret);
+		return -1;
+	}
+
+	datum.data = buf;
+	datum.size = size;
+
+	ret = gnutls_x509_privkey_import(privkey, &datum, GNUTLS_X509_FMT_PEM);
+	if (ret) {
+		gnutls_x509_privkey_deinit(privkey);
+		return -1;
+	}
+
+	ret = x509_get_privkey_id(id, privkey);
+
+	gnutls_x509_privkey_deinit(privkey);
+
+	return ret;
+}
+
+static int try_pubkey(uint8_t *id, uint8_t *buf, int size)
+{
+	gnutls_pubkey_t pubkey;
+	gnutls_datum_t datum;
+	int ret;
+
+	ret = gnutls_pubkey_init(&pubkey);
+	if (ret) {
+		fprintf(stderr, "gnutls_pubkey_init: ");
+		gnutls_perror(ret);
+		return -1;
+	}
+
+	datum.data = buf;
+	datum.size = size;
+
+	ret = gnutls_pubkey_import(pubkey, &datum, GNUTLS_X509_FMT_PEM);
+	if (ret) {
+		gnutls_pubkey_deinit(pubkey);
+		return -1;
+	}
+
+	ret = get_pubkey_id(id, pubkey);
+
+	gnutls_pubkey_deinit(pubkey);
+
+	return ret;
+}
+
+static int read_key_id(uint8_t *id, const char *file)
+{
+	int fd;
+	uint8_t buf[65536];
+	int size;
+	int ret;
+
+	fd = open(file, O_RDONLY);
+	if (fd < 0) {
+		fprintf(stderr, "error opening %s: %s\n", file,
+			strerror(errno));
+		return -1;
+	}
+
+	size = read(fd, buf, sizeof(buf));
+	if (size < 0) {
+		perror("read_key_id: read");
+		close(fd);
+		return -1;
+	}
+
+	close(fd);
 
 	gnutls_global_init();
 
-	ret = x509_read_privkey(&privkey, file, 0);
-	if (ret == 0) {
-		ret = x509_get_privkey_id(id, privkey);
-		gnutls_x509_privkey_deinit(privkey);
-	}
+	ret = try_privkey(id, buf, size);
+	if (ret)
+		ret = try_pubkey(id, buf, size);
 
 	gnutls_global_deinit();
 
